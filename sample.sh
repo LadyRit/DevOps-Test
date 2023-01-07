c9b214fe4114f6c5c9fa3feb7d1d77e5ce26c66d
LadyRit <111931701+LadyRit@users.noreply.github.com>
2023-01-07 07:30:32 -0500

    Update Sample.sh

--- sample.sh
+++ sample.sh
@@ -3,4 +3,4 @@
 
 echo "This is Rita, DevOps Engineer learning git. I know how to resolve git conflict."
 echo "Too many commands to learn"
-echo "I am learning git on $(date)"
+echo "I am learning git on"
